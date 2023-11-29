fdf31622d6f788735aa729d6cdab9ec9fbf60980
Krasilnikov Roman <x0kill@ya.ru>
2023-11-30 00:11:20 +0300

    Sentry migration

--- next.config.js
+++ next.config.js
@@ -11,6 +11,32 @@ const nextConfig = {
 
 module.exports = withSentryConfig(
   nextConfig,
-  { silent: true },
-  { hideSourceMaps: false }
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: 'home-y01',
+    project: 'veterinary-clinic-online',
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
 )
