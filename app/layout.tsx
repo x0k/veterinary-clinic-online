8a8c2b3f3d4ee20e9c1a25202d5cca57a2b0d407
Krasilnikov Roman <x0kill@ya.ru>
2024-03-09 13:44:00 +0300

    Speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from 'react'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from 'next'
 import '@/styles/globals.css'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
